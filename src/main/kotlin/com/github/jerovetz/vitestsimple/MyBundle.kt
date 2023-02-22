c2d8efc95c16ade9bd525fa020489b412906d1bc
GitHub Action <action@github.com>
2023-02-22 14:50:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/jerovetz/vitestsimple/MyBundle.kt
+++ src/main/kotlin/com/github/jerovetz/vitestsimple/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jerovetz.vitestsimple
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jerovetz/vitestsimple/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jerovetz/vitestsimple/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jerovetz.vitestsimple.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jerovetz.vitestsimple.MyBundle
 
 class MyApplicationService {
 
